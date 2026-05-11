e2518a60b08fc9472473741d61b9746f2527cd63
Reece Tunesi <rtunesi2000@gmail.com>
2026-05-11 15:22:27 +0100

    JavaScript learning started

---
+++ Freecodecamp JavaScript Learning/Sentence Maker/script.js
@@ -0,0 +1,23 @@
+// Build a Sentence Maker
+
+let adjective = "small";
+let noun = "boy"; 
+let verb = "cold";
+let place = "house";
+let adjective2 = "tiny"
+let noun2 = "apples";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "tall";
+noun = "girl";
+verb = "hot";
+place = "mansion";
+adjective2 = "large";
+noun2 = "oranges"
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
---
+++ Freecodecamp JavaScript Learning/Trivia Bot/script.js
@@ -0,0 +1,24 @@
+// Build a JavaScript Trivia Bot
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "ChatBot";
+const botLocation = "United Kingdom";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " Is Not the Same as Java: Despite the similar names, " + favoriteLanguage + " and Java are two entirely different languages.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " Was Created in Just 10 Days: JavaScript was developed by Brendan Eich in 1995";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is interpreted, not compiled, which means it runs line-by-line in the browser. This enables real-time feedback and rapid development.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
